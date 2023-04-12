f574d6e91a490a1de0c1cdbdf46ae72150be484b
Waindini Nur Fitri <waindini@gmail.com>
2023-04-12 22:29:45 +0700

    update product

--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,7 @@ const {productController} = require('../controllers/product');
 const { authentication, authorization } = require('../middlewares/auth');
 const { uploader } = require('../middlewares/multer')
 
-router.post('/add',authentication, authorization, uploader.single('picture'), productController.addproduct)
+router.post('/add',authentication, authorization, uploader.single('picture'), productController.addProduct)
 router.get('/list', authentication, productController.productList)
 router.get('/find/:id',authentication, productController.findById)
 router.delete('/delete/:id', authentication, authorization, productController.deleteProduct)
